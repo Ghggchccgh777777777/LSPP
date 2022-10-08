83ccbd0cdb6ed923a7b2e30c77fab1b101cb4519
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2022-10-08 11:26:09 +0800

    Update chuniao.js

--- chuniao.js
+++ chuniao.js
@@ -1,9 +1,9 @@
-//作者伟人q55749353
+
 var hausd0rff = $request.headers;
 
-    hausd0rff['X-TOKEN'] = 'xOIby5DWr2tCf/OxKvnSZoHnD36X8YyzazHwXpj8r+Vqkqa30wWe5PIarCBfLcUuYQfrSQKrtwdb7Un7dQesn3opn4Dw39HpJtFipm7B7Np0SCBYKelhyBMOy9ZKbIhpWA22y6C9NxEQOk9JVEgPj+NsSTs6jq8akUhr8HshbjE6yGSvUSmX2Xh/FEy4vdzvqKi2Tv4Vdg6Pclgl9ATCRu8RH1t1jI9azBln0KW7xALETdzFEKM9WfCvnPGMcyInMlcCB90FQdpGw4LRbeI2edwf9lHkz6dDBDz/JUYJAsayNiPB/BFmE7E2RvZKQ7mnrfGSOzXH8f3jPWBsu2kDJehcqK2I9ESsz7ywh4LdQbhBCNJv5dwMhO3WmAC57lq+wEYnd3UUoLbx0R5eATG9954biHfovg2TyVaJpvn/xmnMfNJQ/xDZFaospj2v21zh0TJbpMsupYm8znKJBVb/C/l9QaI7dOI5T3VNfx35shsmaaaQ5T91kRFR4C7/xKUkjT5nBo0vkVUHDZI3vPyHq7G4W7gpCqv5qTC8jjO+jeIMCHAgoxJc6CehFuC/LRVUXXq3CLp8/CarMONEVaoRNGust65MdXvhlasCrpclkcU6WurTMK9rnyjfqfVx7O3hL01KShBL6k1Mz5IEXVBPw8PMeNvon/O4quSQxnF47sY=';
+    hausd0rff['X-TOKEN'] = 'xOIby5DWr2tCf/OxKvnSZoHnD36X8YyzazHwXpj8r+Vqkqa30wWe5PIarCBfLcUuYQfrSQKrtwdb7Un7dQesn3opn4Dw39HpJtFipm7B7Np0SCBYKelhyBMOy9ZKbIhpWA22y6C9NxEQOk9JVEgPj+NsSTs6jq8akUhr8HshbjH8HZBmS5ur0lIWNl5KADZ0SYrLRBUqjNsl9tLSHyE+QGQbDIqidP4ahsi1CLiCmy2QgrM5pLGgrWJgjudsFIPgYvuZfISacVQhwKXKbY00co+6zKflsztXh7X1rpasaH0PcFqe7DaXscND4YY7FSIwMnNBCu1ryCAtne+uiTHwoVKZpCWDfgnoTM0Kr5O7Re8J184qQWmVsHl4ZrIpOgMnt4Qxzrp/WfTyZeto0u9e17RpJRt4BgTgLxLafQ6Ku/IwHh+5UXhPRv/JNKFO2qIhp+onypxrEXBMXhfLk7WfREXSzBiR9lKW6c6Y14uHOsKukSY99ncIbFPRIR0Y8SBSqwHaz7yUqyILAvWlQT9juF5hxq/VxPmgoRb3/f+HQNFfoJihCXL+vHkc/GUDt+ygvo6PWc3PgpFxigYcEBqCVlaHnMecEFXiBQXJulTq9dWFgooaz4iPCdExiV5saLfrJ4rqZBr57RDWN1GnGRnJFAQ9OET/qsDC+auDRB/+JZU=';
 
-    hausd0rff['User-Agent'] = 'GKDYVideo/1.0 (iPhone; iOS 15.3.1; Scale/2.00)';
+    hausd0rff['User-Agent'] = 'GKDYVideo/1.0 (iPhone; iOS 12.4.4; Scale/2.00)';
     
 
 $done({headers : hausd0rff});
